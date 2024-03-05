ab7dba342c24548b389616882b7cb695613c3cb0
duongbuibd <duongbuibd@gmail.com>
2024-03-05 11:44:19 +0700

    header ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -23,7 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.variable}>{children}</body>
+      <ClerkProvider>
+        <body className={poppins.variable}>{children}</body>
+      </ClerkProvider>
     </html>
   );
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,20 +1,43 @@
-import { Link } from "lucide-react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <header className="w-full border-b">
-      <div className="flex justify-between items-center wrapper">
-        <Link href="/" className="w-36">
+    <header className="w-full border-t">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/">
           <Image
             src="/assets/images/logo.svg"
+            alt="logo"
             width={128}
             height={38}
-            alt="Evently logo"
           />
         </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end items-center gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild size={"lg"} className="rounded-full">
+              <Link href={"/sign-in"}>Log in</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   );
 };
+
 export default Header;
